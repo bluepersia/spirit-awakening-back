7c707c6cb8758296e739db9f047cf761f83055a9
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-02-27 14:01:52 -0600

    Handle 404

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 const app = express ();
 import productRouter from './routes/productRoutes';
 import globalErrorHandler from './controllers/errorController';
+import AppError from './util/AppError';
 
 app.use ('/api/v1/products', productRouter);
 
+app.all ('*', () => { throw new AppError ('Route was not found!', 404)});
 
 app.use (globalErrorHandler);
 
